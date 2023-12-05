30f7bc114502a726e152c5e14d5d01a4debf49d1
Nazeef Ahmad <nazeefahmad555@gmail.com>
2023-12-05 11:12:24 +0530

    commit

---
+++ Basics/oddEven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int a;
+    cout <<"Enter the Number\n45";
+    cin >>a;
+    if(a%2==0){
+        cout << "Numbe is Even\n";
+
+    } 
+    else {
+        cout << "Number is Odd\n";
+    }
+}
